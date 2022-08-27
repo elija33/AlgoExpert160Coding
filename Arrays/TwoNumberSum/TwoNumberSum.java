be0f6cdccbda1b91b47b43678d6d335089c14ba4
elija33 <amponsaeli@gmail.com>
2022-08-26 20:25:13 -0400

    finding two number sum

--- Arrays/TwoNumberSum/TwoNumberSum.java
+++ Arrays/TwoNumberSum/TwoNumberSum.java
@@ -1,21 +1,39 @@
 package TwoNumberSum;
 
+import java.util.Scanner;
+
 public class TwoNumberSum{
     public static int[] twoNumberSum(int[] array, int targetSum){
         for(int i = 0; i<=array.length; i++){
             for(int j = i + 1; j<=array.length; j++){
                 if(array[i] + array[j] == targetSum){
-                    System.out.println("Two Number Sum pair " + targetSum + "is (" + array[i] + ", " + array[j] + ")");
+                    return new int[] {i, j};
                 }
             }
         }
-        return array;
+        return new int[] {};
     }
     public static void main(String[] args) {
-        int array[] = {0,-1,2,-3,1,8};
-        int targetSum = 10;
-        if(twoNumberSum(array, targetSum)){
-            System.out.println("Valid pair exists");
+        Scanner number = new Scanner(System.in);
+
+        int numbers = number.nextInt();
+        int[] array = new int[numbers];
+
+        for(int i = 0; i < numbers; i++){
+            array[i] = number.nextInt();
+        }
+
+        int targetSum = number.nextInt();
+
+            number.close();
+
+            int[] first = twoNumberSum(array, targetSum);
+
+            if(first.length == 2) {
+                System.out.println(first[0] + " " + first[1]);
+            }
+            else{
+                System.out.println("No solution found!");
+            }
         }
     }
-}
