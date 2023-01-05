f9d638991a9b504aaf8346fffb6426f2030187c4
Elija33 <amponsaeli@gmail.com>
2023-01-05 16:52:56 -0500

    Search for Range and QuickSelect

---
+++ QuickSelect/Select.java
@@ -0,0 +1,5 @@
+package QuickSelect;
+
+public class Select {
+    
+}
--- SearchforRange/Range.java
+++ SearchforRange/Range.java
@@ -5,14 +5,23 @@ public class Range {
         int start = 0;
         int end = array.length-1;
         while(start <= end){
-            int mid = (start + end) / 2;
-            if(array[mid] == target)
+            if(array[start] < target){
+                start++;
+            }
+            else if(array[end] > target){
+                end--;
+            }
+            if(array[start] == target && array[end] == target){
+                return new int[] {start, end};
+            }
         }
         return new int[] {-1, -1};
     }
     // 0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73
-                              ^
+    //                        ^
     public static void main(String[] args) {
-        
+        int array[] = {0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73};
+        int target = 45;
+        System.out.println(Search(array, target));
     }
 }
